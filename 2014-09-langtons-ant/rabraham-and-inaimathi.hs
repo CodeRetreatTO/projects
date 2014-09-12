d9eddbc01d722ab8032608987e3cb1bf6330d04a
inaimathi <inaimathi@self>
2014-09-11 23:08:23 -0400

    -Printing is now a thing

--- 2014-09-langtons-ant/rabraham-and-inaimathi.hs
+++ 2014-09-langtons-ant/rabraham-and-inaimathi.hs
@@ -1,6 +1,7 @@
 module Langton where
 
 import Prelude hiding (Left, Right)
+import Data.Set (Set(..), member, insert, delete, fromList)
 
 data Direction = Up | Right | Down | Left deriving (Eq, Enum, Show)
 
@@ -14,16 +15,15 @@ left dir = pred dir
 
 data Ant = Ant Int Int Direction deriving (Eq, Show)
 data World = World [Ant] Coords deriving (Show)
-type Coords = [(Int, Int)]
-
+type Coords = Set (Int, Int)
 
 flipCell :: (Int, Int) -> Coords -> Coords
-flipCell cell coords= if cell `elem` coords
-                  then filter (/=cell) coords
-                  else cell:coords
+flipCell cell coords= if cell `member` coords
+                  then cell `delete` coords
+                  else cell `insert` coords
 
 turn :: Coords -> Ant -> Ant
-turn coords (Ant x y dir) = Ant x y $ if (x, y) `elem` coords
+turn coords (Ant x y dir) = Ant x y $ if (x, y) `member` coords
                                       then left dir
                                       else right dir
 
@@ -38,6 +38,15 @@ step (World ants coords) = World newAnts newCoords
     where newCoords = foldl (\memo (Ant x y _) -> flipCell (x, y) memo) coords ants
           newAnts = map (onwards . turn coords) ants
 
-test = World [(Ant 4 4 Up), (Ant 3 7 Left)] []
+test = World [(Ant 4 4 Up), (Ant 3 7 Left)] $ fromList []
 
-main = mapM_ (putStrLn . show) . take 10 $ iterate step test
+showWorld :: (Int, Int) -> World -> String
+showWorld (w, h) (World ants coords) = unlines [line y | y <- [0..h]]
+    where line y = [charOf (x, y) | x <- [0..w]]
+          antCells = map (\(Ant x y _) -> (x, y)) ants
+          charOf cell
+              | cell `elem` antCells = '+'
+              | cell `member` coords = 'O'
+              | otherwise = ' '
+
+main = mapM_ (putStrLn . showWorld (10, 10)) . take 40 $ iterate step test
