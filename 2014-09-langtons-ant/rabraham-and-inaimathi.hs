d7a6cb308bc91ba92001c66886ebe3fec1ad828b
inaimathi <inaimathi@self>
2014-09-24 23:26:32 -0400

    -Re-wrote the animate function away with loop

--- 2014-09-langtons-ant/rabraham-and-inaimathi.hs
+++ 2014-09-langtons-ant/rabraham-and-inaimathi.hs
@@ -3,13 +3,14 @@ module Main where
 import Prelude hiding (Left, Right)
 
 import Haste
+import Haste.Concurrent
 import Data.Set (Set(..), member, insert, delete, fromList)
 import qualified Data.Set as Set
 import Data.Maybe
 import qualified Data.Map as Map
 
 data Direction = Up | Right | Down | Left deriving (Eq, Enum, Show)
-
+               
 right :: Direction -> Direction 
 right Left = Up
 right dir = succ dir
@@ -18,23 +19,29 @@ left :: Direction -> Direction
 left Up = Left
 left dir = pred dir
 
-data Ant = Ant (Int, Int) Direction deriving (Eq, Show)
+data Ant = Ant Point Direction deriving (Eq, Show)
 data World = World [Ant] Coords deriving (Show)
-type Coords = Set (Int, Int)
+type Coords = Set Point
 
-ptMin (x, y) (x', y') = (min x x', min y y')
-ptMax (x, y) (x', y') = (max x x', max y y')
+data Point = Point Int Int deriving (Eq, Show)
 
-bordersOf :: World -> ((Int, Int), (Int, Int))
-bordersOf (World ants coords) = (ptMin min min', ptMax max max')
-    where (min, max) = Set.foldl (\(min, max) pt -> (ptMin min pt, ptMax max pt)) zero coords
-          (min', max') = foldl (\(min, max) (Ant pt _) -> (ptMin min pt, ptMax max pt)) zero ants
-          zero = ((0, 0), (0, 0))
+instance Ord Point where
+    (Point x y) `compare` (Point x' y') = case x `compare` x' of
+                                            EQ -> y `compare` y'
+                                            res -> res
+    min (Point x y) (Point x' y') = Point (min x x') (min y y')
+    max (Point x y) (Point x' y') = Point (max x x') (max y y')
 
-flipCell :: (Int, Int) -> Coords -> Coords
+bordersOf :: World -> (Point, Point)
+bordersOf (World ants coords) = (min lo lo', max hi hi')
+    where (lo, hi) = Set.foldl (\(lo, hi) pt -> (min lo pt, max hi pt)) zero coords
+          (lo', hi') = foldl (\(lo, hi) (Ant pt _) -> (min lo pt, max hi pt)) zero ants
+          zero = ((Point 0 0), (Point 0 0))
+
+flipCell :: Point -> Coords -> Coords
 flipCell cell coords= if cell `member` coords
-                  then cell `delete` coords
-                  else cell `insert` coords
+                      then cell `delete` coords
+                      else cell `insert` coords
 
 turn :: Coords -> Ant -> Ant
 turn coords (Ant pt dir) = Ant pt $ if pt `member` coords
@@ -42,10 +49,10 @@ turn coords (Ant pt dir) = Ant pt $ if pt `member` coords
                                     else right dir
 
 onwards :: Ant -> Ant
-onwards (Ant (x, y) Up) = Ant (x, (pred y)) Up
-onwards (Ant (x, y) Right) = Ant ((succ x), y) Right
-onwards (Ant (x, y) Down) = Ant (x, (succ y)) Down
-onwards (Ant (x, y) Left) = Ant ((pred x), y) Left
+onwards (Ant (Point x y) Up) = Ant (Point x (pred y)) Up
+onwards (Ant (Point x y) Right) = Ant (Point (succ x) y) Right
+onwards (Ant (Point x y) Down) = Ant (Point x (succ y)) Down
+onwards (Ant (Point x y) Left) = Ant (Point (pred x) y) Left
 
 step :: World -> World
 step (World ants coords) = World newAnts newCoords
@@ -56,8 +63,8 @@ step (World ants coords) = World newAnts newCoords
 ----- Pretty-print a world state
 showWorld :: World -> String
 showWorld world@(World ants coords) = unlines [line y | y <- [minY..maxY]]
-    where line y = [charOf (x, y) | x <- [minX..maxX]]
-          ((minX, minY), (maxX, maxY)) = bordersOf world
+    where line y = [charOf (Point x y) | x <- [minX..maxX]]
+          ((Point minX minY), (Point maxX maxY)) = bordersOf world
           antCells = Map.fromList $ map (\(Ant pt dir) -> (pt, dir)) ants
           charDir Up = '↑'
           charDir Right = '→'
@@ -68,22 +75,17 @@ showWorld world@(World ants coords) = unlines [line y | y <- [minY..maxY]]
               | cell `member` coords = 'O'
               | otherwise = ' '
 
------ Haste stuff
+----- Utility functions
 setContent :: ElemID -> String -> IO ()
 setContent id newContent = withElem id (\e -> setProp e "innerHTML" newContent)
 
-animate :: Int -> World -> Int -> IO ()
-animate delay world steps = setTimeout delay $ recur world steps
-    where puts ct w = do setContent "world" $ showWorld w
-                         setContent "generations" $ show (steps - ct)
-          recur world 0 = setTimeout delay $ puts 0 world
-          recur world ct = do puts ct world
-                              setTimeout delay $ recur (step world) $ pred ct
-
+loop :: Monad m => (a -> m a) -> a -> m b
+loop f a = do res <- f a
+              loop f res
 
 ----- Test data and main
 test :: World
-test = World [(Ant (4, 4) Up), (Ant (3, 7) Left), (Ant (15, 12) Down)] $ fromList []
+test = World [(Ant (Point 4 4) Up), (Ant (Point 3 7) Left), (Ant (Point 15 12) Down)] $ fromList []
 
 serverDemo :: IO ()
 serverDemo = mapM_ print . take 20 $ iterate step test
@@ -91,4 +93,8 @@ serverDemo = mapM_ print . take 20 $ iterate step test
                        putStrLn "--------------------"
 
 main :: IO ()
-main = animate 10 test 4000
+main = concurrent $ loop nextFrame (test, 0)
+    where nextFrame (w, ct) = do liftIO $ setContent "world" $ showWorld w
+                                 liftIO $ setContent "generations" $ show ct
+                                 wait 50
+                                 return $ (step w, succ ct)
