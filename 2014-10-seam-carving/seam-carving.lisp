c696596d2156c8d24fbafe68f915cb88fd3ba2cf
inaimathi <inaimathi@self>
2014-10-16 09:51:32 -0400

    -Mostly working, but butt fugly seam carving algorithm (mutation ... mutation everywhere)

--- 2014-10-seam-carving/seam-carving.lisp
+++ 2014-10-seam-carving/seam-carving.lisp
@@ -1,3 +1,33 @@
+;;;;; Seam-related stuff
+(defclass seam ()
+  ((weight :accessor weight :initform 0 :initarg :weight)
+   (indices :accessor indices :initform nil :initarg :indices)
+   (index-count :accessor index-count :initform 0 :initarg :index-count)
+   (last-index :accessor last-index :initform nil :initarg :last-index)))
+
+(defun make-seam (&optional initial-index/weight-pairs)
+  (multiple-value-bind (ixs w ct) (loop for (ix w) on initial-index/weight-pairs by #'cddr
+				     collect ix into indices sum w into weight sum 1 into index-ct
+				     finally (return (values indices weight index-ct)))
+    (make-instance 'seam :indices ixs :index-count ct :weight w :last-index (last ixs))))
+
+(defmethod copy ((s seam))
+  (let ((ixs (copy-list (indices s))))
+    (make-instance 'seam :indices ixs :index-count (index-count s)
+		   :weight (weight s) :last-index (last ixs))))
+
+(defmethod add-cell! ((seam seam) (index cons) (weight number))
+  (let ((s (copy seam)))
+    (setf (cdr (last-index s)) (list index)
+	  (last-index s) (cdr (last-index s)))
+    (incf (weight s) weight)
+    (incf (index-count s))
+    s))
+
+;;;;; Basic utility
+(defmethod take ((count number) (lst list))
+  (loop for elem in lst repeat count collect elem))
+
 ;;;;; IO Routines
 (defmethod get-lines ((fname string))
   (with-open-file (s fname)
@@ -17,19 +47,33 @@
    lines))
 
 (defmethod seams-in ((e-map list))
-  (sort
-   (loop for ln in (apply #'mapcar #'list e-map)
-      for i from 0
-      collect (make-instance 'seam :weight (apply #'+ ln) :indices (list i)))
-   #'< :key #'weight))
+  (let ((seams (loop for w in (first e-map) for i from 0
+		  collect (make-seam `((,i 0) ,w)))))
+    (loop for ln in (rest e-map) for ln-ix from 1
+       do (setf seams
+		(loop for w in ln for w-ix from 0
+		   append (let* ((seam 
+				  (first 
+				   (sort (remove-if-not
+					  (lambda (s) (<= (- w-ix 1) (caar (last-index s)) (+ w-ix 1)))
+					  seams)
+					 #'< :key #'weight))))
+			    (when seam (list (add-cell! seam `(,w-ix ,ln-ix) w)))))))
+    (remove-duplicates 
+      (sort seams #'< :key #'weight)
+      :from-end t
+      :key (lambda (s) (caar (indices s))))))
 
-(defmethod remove-single-seam! ((lines list) (seam number))
-  (mapc 
-   (lambda (ln)
-     (setf (char ln seam) #\space))
-   lines)
+(defmethod mask-seam! ((lines list) (s seam) &optional (mask #\space))
+  (loop for ln in lines for ix in (indices s)
+     do (setf (char ln (first ix)) mask))
   lines)
 
-(defmethod remove-seams! ((lines list) (seams list))
-  (mapc (lambda (s) (remove-single-seam! lines s)) seams)
-  lines)
+(defmethod remove-seam! ((lines list) (s seam))
+  (mapcar (lambda (ln) (remove #\space ln))
+	  (mask-seam! lines s #\space)))
+
+(defmethod scale-by! ((lines list) (count number))
+  (loop with lns = lines repeat count
+     do (setf lns (remove-seam! lns (first (seams-in (energy-map lns)))))
+     finally (return lns)))
