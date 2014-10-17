02acbce6baf8964e94f26c9c54ff03a33fff301e
inaimathi <inaimathi@self>
2014-10-17 10:06:25 -0400

    -Working seam picker

--- 2014-10-seam-carving/inaimathi.hs
+++ 2014-10-seam-carving/inaimathi.hs
@@ -4,22 +4,20 @@ import Data.Char (ord)
 import Data.List (sortBy)
 import Data.Function (on)
 
-type Score = [[Integer]]
-
 scoreLine :: String -> [Integer]
 scoreLine line = map total zipped
     where l = map (toInteger . ord) line
           zipped = zip3 (head l:l) l $ tail l
           total (a, b, c) = abs (a-b) + abs (b-c)
 
-scoreGrid :: [String] -> Score
+scoreGrid :: Image -> Score
 scoreGrid ls = map scoreLine ls
 
-maskSeam :: [String] -> Int -> [String]
-maskSeam lns ix = map (replace ' ' ix) lns
+maskSeam :: Image -> Seam -> Image
+maskSeam = applySeam (replace ' ')
 
-carveSeam :: [String] -> Int -> [String]
-carveSeam lns ix = map (remove ix) lns
+carveSeam :: Image -> Seam -> Image
+carveSeam = applySeam remove
 
 freshSeams :: [Integer] -> [Seam]
 freshSeams ln = map (\(ix, w) -> Seam w [ix]) $ zip [1..] ln
@@ -28,20 +26,29 @@ seamsIn :: Score -> [Seam]
 seamsIn [] = []
 seamsIn s = sortByWeight allSeams
     where allSeams = foldl (\memo ln -> line ln memo) (freshSeams $ head s) $ tail s
-          line ln seams = concatMap (take 1) $ map (\(ix, w) -> choose ix w seams) $ zip [0..] ln
-          choose ix w seams = map (\s -> add s w ix) . take 1 . sortByWeight $ potentials ix seams
+          line ln seams = map (\(ix, w) -> choose ix w seams) $ zip [0..] ln
+          choose ix w seams = map (\seam -> add seam w ix) . take 1 . sortByWeight $ potentials ix seams
           potentials ix seams = take 3 $ drop (max 0 (fromIntegral ix-1)) seams
 
-sortByWeight :: [Seam] -> [Seam]
-sortByWeight = sortBy (compare `on` weight)
+scaleBy :: Image -> Int -> Image
+scaleBy pic count = head $ drop count $ iterate scaleOne pic
+    where scaleOne lns = case seamsIn $ scoreGrid lns of
+                           [] -> lns
+                           (cheapest:_) -> carveSeam lns cheapest
 
 main :: IO ()
 main = do f <- fmap lines $ readFile "scene.txt"
-          mapM_ putStrLn $ maskSeam f 3
---          putStrLn $ show $ scoreGrid f
+          mapM_ putStrLn $ scaleBy f 10
+          putStrLn " "
+          mapM_ putStrLn $ scaleBy f 30
+          putStrLn " "
+          mapM_ putStrLn $ scaleBy f 50
 
------ Seams
-data Seam = Seam { weight :: Integer, sIxs :: [Integer ] } deriving (Eq, Ord, Show) 
+----- Types and related minutia
+type Score = [[Integer]]
+type Image = [String]
+
+data Seam = Seam { weight :: Integer, sIxs :: [Integer] } deriving (Eq, Ord, Show) 
 
 add :: Seam -> Integer -> Integer -> Seam
 add (Seam w ixs) newWeight newIx = Seam (w + newWeight) $ newIx:ixs
@@ -50,6 +57,12 @@ indices :: Seam -> [Integer]
 indices (Seam _ ixs) = reverse ixs
 
 ----- Utility
+applySeam :: (Integer -> String -> String) -> Image -> Seam -> Image
+applySeam fn lns seam = map (\(ix, ln) -> fn ix ln) $ zip (indices seam) lns
+
+sortByWeight :: [Seam] -> [Seam]
+sortByWeight = sortBy (compare `on` weight)
+
 remove :: (Num i, Enum i, Eq i) => i -> [a] -> [a]
 remove _ [] = []
 remove 0 (_:rest) = rest
