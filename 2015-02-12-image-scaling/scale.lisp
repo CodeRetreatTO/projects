95ca3e710902a1613cc1c9f298cd9f267fe234d9
inaimathi <inaimathi@self>
2015-02-12 20:03:43 -0500

    -Revised lisp version

--- 2015-02-12-image-scaling/scale.lisp
+++ 2015-02-12-image-scaling/scale.lisp
@@ -1,9 +1,6 @@
 (defun nearest-neighbor (scale input)
-  (mapcan 
-   (lambda (ln) 
-     (make-list scale :initial-element 
-		(mapcan 
-		 (lambda (c)
-		   (make-list scale :initial-element c))
-		 ln)))
-	  input))
+  (flet ((stretch (thing) (make-list scale :initial-element thing)))
+    (mapcan
+     (lambda (ln)
+       (stretch (mapcan (lambda (c) (stretch c)) ln)))
+     input)))
