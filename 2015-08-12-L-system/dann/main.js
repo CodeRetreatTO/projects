c74c94bfea4c58d8857bd629ee207d953dab9b6d
dann toliver <dann@bentobox.net>
2015-08-13 00:50:59 -0400

    New interface for controlling the l-systems

--- 2015-08-12-L-system/dann/main.js
+++ 2015-08-12-L-system/dann/main.js
@@ -1,5 +1,3 @@
-var start = 'a'
-
 var pretty_rules = { 'a' : '+b-a-b+'
                    , 'b ' : '-a+b+a-'
                    // , 'b' : 'a'
@@ -9,46 +7,88 @@ var pretty_rules = { 'a' : '+b-a-b+'
 
 var rules = transform_rules(pretty_rules)
 
-
-
 function transform_rules(rules) {
-    var keys = Object.keys(rules)
-    return keys.map(function(key) {
-        old_key = key
-        key = key.trim()
-        return { replacement: rules[old_key]
-               , key:    key
-               , char:   key.split('>')[0].split('<').reverse()[0]
-               , after:  key.split('>')[1] || ""           // get after  context
-               , before: key.split('<').reverse()[1] || "" // get before context
-               }
-    })
+  var keys = Object.keys(rules)
+  return keys.map(function(key) {
+    var old_key = key
+    key = key.trim()
+    return { replacement: rules[old_key]
+           , key:    key
+           , char:   key.split('>')[0].split('<').reverse()[0]
+           , after:  key.split('>')[1] || ""           // get after  context
+           , before: key.split('<').reverse()[1] || "" // get before context
+           }
+  })
 }
+
+function rule_transformer(rules) {
+  // haha okay so this is ridiculous
+  return rules.reduce(function(acc, r, i) {
+    var lhs = r.split('->')[0].trim()
+    var rhs = r.split('->')[1].trim()
+    acc[lhs + " ".repeat(i)] = rhs
+    return acc
+  }, {})
+}
+
+
+var elements = ['rule1', 'rule2', 'rule3', 'rule4', 'rule5', 'start', 'angle', 'size', 'iterations']
+var els = {}
+var elvals = {}
+
+init()
+
+function init() {
+  getEls()
+  render(lsystem('a', 8), deg_to_rad(60), 3)
+}
+
+function getEls() {
+  elements.forEach(function(name) {
+    els[name] = document.getElementById(name)
+  })
+}
+
+function readEls() {
+  var elvals = {}
+  elements.forEach(function(name) {
+    elvals[name] = els[name].value
+  })
+  return elvals
+}
+
+document.getElementById('menu').addEventListener('keyup', function(e) {
+  var elvals = readEls()
+  rules = Object.keys(elvals).slice(0, 5).map(function(name) {return elvals[name]}).filter(Boolean)
+  rules = transform_rules(rule_transformer(rules))
+  render(lsystem(elvals['start'], +elvals['iterations']), deg_to_rad(+elvals['angle']), +elvals['size'])
+})
 
 
 function step(s) {
-   return s.split('')
-           .map(apply_rules)
-           .join('')
+  return s.split('')
+          .map(apply_rules)
+          .join('')
 }
 
 function apply_rules(char, index, array) {
-    var matches = find_matching_rules(char, index, array)
-    var output = pick_and_apply_rule(matches) || char
-    return output
+  var matches = find_matching_rules(char, index, array)
+  return pick_and_apply_rule(matches) || char
 }
 
 function find_matching_rules(c, i, a) {
-    return rules.filter(function(rule) {
-        return rule.char   == c
-            && rule.after  == a.slice(i+1, i+1+rule.after.length).join('')
-            && rule.before == a.slice(i-rule.before.length, i   ).join('')
-    })
+  return rules.filter(function(rule) {
+    return rule.char   == c
+        && rule.after  == a.slice(i+1, i+1+rule.after.length).join('')
+        && rule.before == a.slice(i-rule.before.length, i   ).join('')
+  })
 }
 
 function pick_and_apply_rule(matches) {
-    return (matches.sort(function(a, b) {
-        return (b.key.length - a.key.length) || (Math.random() - 0.5) })[0]||{}).replacement
+  return (matches.sort(function(a, b) {
+    return (b.key.length - a.key.length)
+        || (Math.random() - 0.5) })[0]||{})
+    .replacement
 }
 
 function lsystem(s, n) {
@@ -71,8 +111,8 @@ function render(str, rad, size) {
 
   ctx.clearRect(0, 0, 2000, 2000)
 
-  var x = 0
-  var y = 0
+  var x = 20
+  var y = 20
   var tau = 2*Math.PI
   var angle = 0
 
