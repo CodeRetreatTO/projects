90f96da6634d773e0a561ff5ccdd33f7347d54ba
inaimathi <inaimathi@self>
2015-12-10 12:40:04 -0500

    - minor cosmetics on quilt.hs

--- 2015-12-quilt/dxnn_inaimathi/quilt.hs
+++ 2015-12-quilt/dxnn_inaimathi/quilt.hs
@@ -14,42 +14,43 @@ findSteps :: Quilt -> [[(Vertex, Vertex)]]
 findSteps q = recur [q]
     where recur [] = []
           recur qs
-              | [] == filter noEdges qs = recur $ concatMap nextSteps qs
-              | otherwise = concat [map (reverse . steps) qs, recur $ filter (not . noEdges) qs]
-          noEdges q' = [] == edges q'
+              | [] == filter isDone qs = recur $ concatMap nextSteps qs
+              | otherwise = concat [map (reverse . steps) qs, recur $ filter (not . isDone) qs]
 
 nextSteps :: Quilt -> [Quilt]
 nextSteps q = catMaybes . map stitch . nub $ edges q
-    where stitch (Edge _ a b) = case res of
-                                  Just r -> Just $ r { steps = (a, b) : (steps r)}
-                                  Nothing -> Nothing
-              where res = combineVertices q a b
+    where stitch (Edge _ a b) = combineVertices q a b
 
 combineVertices :: Quilt -> Vertex -> Vertex -> Maybe Quilt
 combineVertices q v v' = case nub . map dirOf . filter relevantE $ newEs of
-                           [_] -> Just $ q { vertices = newVs, edges = filter (not . relevantE) $ newEs }
+                           [_] -> Just $ q { vertices = newVs
+                                           , edges = filter (not . relevantE) $ newEs
+                                           , steps = (v, v') : (steps q)}
                            _ -> Nothing
     where newVertex = v ++ v'
           newVs = newVertex : (filter (\e -> not $ e `elem` [v, v']) $ vertices q)
-          newEs = map replaceAs . map replaceBs $ edges q
-          relevantE (Edge _ a b)
-              | and [a == newVertex, b == newVertex] = True
-              | otherwise = False
-          replaceAs e@(Edge dir a b)
-              | a `elem` [v, v'] = Edge dir newVertex b
-              | otherwise = e
-          replaceBs e@(Edge dir a b)
-              | b `elem` [v, v'] = Edge dir a newVertex
-              | otherwise = e
+          newEs = map (replaceV v newVertex) . map (replaceV v' newVertex) $ edges q
+          relevantE (Edge _ a b) = and [a == newVertex, b == newVertex]
 
 ---------- Datatypes
 type Vertex = String
 data Direction = U | O deriving (Eq, Show)
 data Edge = Edge Direction Vertex Vertex deriving (Eq, Show)
-data Quilt = Quilt { edges :: [Edge], vertices :: [Vertex], steps :: [(Vertex, Vertex)] } deriving (Eq, Show)
+
+replaceV :: Vertex -> Vertex -> Edge -> Edge
+replaceV old new edge@(Edge dir a b)
+    | and [a == old, b == old] = Edge dir new new
+    | a == old = Edge dir new b
+    | b == old = Edge dir a new
+    | otherwise = edge
 
 dirOf :: Edge -> Direction
 dirOf (Edge d _ _) = d
+
+data Quilt = Quilt { edges :: [Edge], vertices :: [Vertex], steps :: [(Vertex, Vertex)] } deriving (Eq, Show)
+
+isDone :: Quilt -> Bool
+isDone q = [] == edges q
 
 ---------- Test data
 mkTest :: String -> [Edge] -> Quilt
